a47b55bcdf3c6118b901691d845fbb619a79db9f
Ian Calloway <iccalloway92@gmail.com>
2020-07-11 15:09:41 -0400

    Removing Extras

--- populate_list.js
+++ populate_list.js
@@ -7,7 +7,7 @@ options.send(null);
 options.onreadystatechange = function(){
     if (options.readyState == 4 && options.status == 200){
         data_list = JSON.parse(options.response);
-        console.log("data_list loaded")
+        //console.log("data_list loaded")
         for(key in data_list){
             var new_option = document.createElement('option');
             new_option.value = key;
